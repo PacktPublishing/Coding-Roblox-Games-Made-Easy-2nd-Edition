b2000dee4d94bbfc2dc165c370c7583b12029cde
Zander Brumbaugh <zanderbrumbaugh@gmail.com>
2022-03-29 11:21:14 -0700

    Update LocalHandler.lua

--- Chapter06/Client/LocalHandler.lua
+++ Chapter06/Client/LocalHandler.lua
@@ -1,14 +1,7 @@
-local returnModFunc = script.ReturnMod
-local modules = {}
-
 for _, module in pairs(script:GetChildren()) do
 	if module:IsA("ModuleScript") then
-		task.defer(function()
-			modules[module.Name] = require(module)
+		task.spawn(function()
+			require(module)
 		end)
 	end
-end
-
-returnModFunc.OnInvoke = function(name)
-	return modules[name] or require(script:FindFirstChild(name))
 end
